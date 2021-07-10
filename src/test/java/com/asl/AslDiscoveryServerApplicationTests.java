df045b59ae657808189d0bc4d42935d8bad1b900
zubayer <zubayer@ZUBAYER>
2021-07-10 10:02:27 +0600

    1st commit

---
+++ src/test/java/com/asl/AslDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AslDiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
